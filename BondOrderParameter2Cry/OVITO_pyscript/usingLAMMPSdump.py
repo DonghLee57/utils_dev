fee285859ac088a15e68a721ccdc34042ee52174
DongYoz Lee <zwqa1234@snu.ac.kr>
2019-07-02 10:31:35 +0900

    fix bug and add the option to remove isolated crystalline atoms

--- BondOrderParameter2Cry/OVITO_pyscript/usingLAMMPSdump.py
+++ BondOrderParameter2Cry/OVITO_pyscript/usingLAMMPSdump.py
@@ -1,24 +1,24 @@
-#python script for OVITO
-#sc: tags are considered once.
-#dc: tags are considered upto the its first neighbors.
-
 from ovito.data import *
 import numpy as np
 
 def modify(frame, data):
-    looptag = ['sc','dc']
-    tag = 'dc'
+    looptag = ['sc','dc','dc-isolated']
+    tag = 'dc-isolated'
     cols = ['c_tot[3]','c_tot[5]']
     Q8 = data.particles[cols[0]]
     Q12 = data.particles[cols[1]]
 
-    cr_val = 0.20
+    cr_val = 0.21
     cutoff = 3.2
     n_nei  = 3
     c_code = [0.2, 0.6, 0.8] #R, G, B
 
-    colors = np.ones((data.particles.count, 3))
-    data.particles_.create_property('Color', data=colors)
+    #colors = np.ones((data.particles.count, 3))
+    #data.particles_.create_property('Color', data=colors)
+    trans = np.ones(data.particles.count)
+    data.particles_.create_property('Transparency', data=trans)
+
+    #
     tag1 = []
     tag2 = []
 
@@ -33,7 +33,8 @@ def modify(frame, data):
     if tag == looptag[0]:#Consider the atom itself
         for idx in range(data.number_of_particles):
             if tag1[idx] and tag2[idx]:
-                colors[idx] = np.array(c_code)
+                #colors[idx] = np.array(c_code)
+                trans[idx] = np.array(0)
     elif tag == looptag[1]:#Consider the first neigbors of the atom
         finder = CutoffNeighborFinder(cutoff, data)
         for idx in range(data.number_of_particles):
@@ -42,6 +43,25 @@ def modify(frame, data):
                 if tag1[neigh.index] and tag2[neigh.index]: c += 1
                 else: break
             if c >= n_nei:
-                colors[idx] = np.array(c_code)
-
-   data.particles_.create_property('Color', data=colors)
+                #colors[idx] = np.array(c_code)
+                trans[idx] = np.array(0)
+    elif tag == looptag[2]:#After considering the first neighbors, remove isolated crystalline atoms
+        checker = [False]*data.number_of_particles
+        finder = CutoffNeighborFinder(cutoff, data)
+        for idx in range(data.number_of_particles):
+            c = 0
+            for neigh in finder.find(idx):
+                if tag1[neigh.index] and tag2[neigh.index]: c += 1
+            if c >= n_nei:
+                checker[idx] = True
+                #colors[idx] = np.array(c_code)
+                trans[idx] = np.array(0)
+        for idx in range(data.number_of_particles):
+            test =[]
+            for neigh in finder.find(idx):
+                test.append(checker[neigh.index])
+            if True not in test:
+                trans[idx] = np.array(1) 
+                
+    #data.particles_.create_property('Color', data=colors)
+    data.particles_.create_property('Transparency', data=trans)
