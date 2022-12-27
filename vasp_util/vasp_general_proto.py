5b57efd47437a38828c4c5e2f7a828722d57ac43
Dongheon <zwqa1234@snu.ac.kr>
2022-12-27 13:54:00 +0900

    Update vasp_general_proto.py

--- vasp_util/vasp_general_proto.py
+++ vasp_util/vasp_general_proto.py
@@ -8,36 +8,42 @@
 #  3) bin in x,y, or z direction
 #  4) plot pdos of the allocated atoms
 #
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    #test for "readPOSCAR" function
-    #print(info_pos)
-    lat, types, atoms, *info_pos = readPOSCAR('POSCAR')
-    print(fit_density(8.14,lat, types, atoms))
+    FLAG = 'poscar'
 
-    #test for "readDOSCAR" function
-    #ISPIN = 2
-    #mask_u = np.arange(1,19,2)
-    #mask_d = np.arange(2,19,2)
-    #TDOS, PDOS, fermi = readDOSCAR("DOSCAR", ISPIN)
-    #
-    #fig, ax = plt.subplots()
-    #ax.axhline(0,c='k')
-    #ax.axvline(0,c='k',ls=':')
-    #ax.plot(TDOS.T[0]-fermi,TDOS.T[1],'r')
-    #ax.plot(TDOS.T[0]-fermi,TDOS.T[2]*(-1),'r')
-    #ax.plot(PDOS[-1].T[0]-fermi,np.sum(PDOS[-1][:].T[mask_u],axis=0),'b')
-    #ax.plot(PDOS[-1].T[0]-fermi,np.sum(PDOS[-1][:].T[mask_d],axis=0)*(-1),'b')
-    #ax.set_xlim([-2,2])
-    #ax.set_xlabel(r'E-E$_{f}$')
-    #ax.set_ylabel('a.u.')
+    if FLAG.lower() == 'poscar':
+        # POSCAR related jobs
+        #lat, types, atoms, Dcoords, Ccoords = readPOSCAR(sys.argv[1])
+        pos_info = readPOSCAR(sys.argv[1])
+        pos_info[-2][:,2] = pos_info[-2][:,2] -0.25
+        writePOSCAR('./test.vasp',*pos_info)
 
-    #plt.tight_layout()
-    #plt.show()
+    elif FLAG.lower() == 'doscar':
+        # DOSCAR related jobs
+        ISPIN = 2
+        mask_u = np.arange(1,19,2)
+        mask_d = np.arange(2,19,2)
+        #TDOS, PDOS, fermi = readDOSCAR("DOSCAR", ISPIN)
+        #
+        #fig, ax = plt.subplots()
+        #ax.axhline(0,c='k')
+        #ax.axvline(0,c='k',ls=':')
+        #ax.plot(TDOS.T[0]-fermi,TDOS.T[1],'r')
+        #ax.plot(TDOS.T[0]-fermi,TDOS.T[2]*(-1),'r')
+        #ax.plot(PDOS[-1].T[0]-fermi,np.sum(PDOS[-1][:].T[mask_u],axis=0),'b')
+        #ax.plot(PDOS[-1].T[0]-fermi,np.sum(PDOS[-1][:].T[mask_d],axis=0)*(-1),'b')
+        #ax.set_xlim([-2,2])
+        #ax.set_xlabel(r'E-E$_{f}$')
+        #ax.set_ylabel('a.u.')
+        #plt.tight_layout()
+        #plt.show()
 
-    #info = readPROCAR(winPATH + "PROCAR_example")
+    elif FLAG.lower() == 'outcar':
+        out_info = readOUTCAR(sys.argv[1])
 
     return 1
 
@@ -85,6 +91,22 @@ def readPOSCAR(filename):
             Dcoords = np.matmul(Ccoords,ilat)
     return lat, types, atoms, Dcoords, Ccoords
 
+def writePOSCAR(filename,lat,types,atoms,Dcoords,Ccoords):
+    content  ='Re-write POSCAR\n'
+    content +=' 1.0\n'
+    for i in range(3):
+        content += f'{" ":^6}{lat[i][0]:^20.12f}{lat[i][1]:^20.12f}{lat[i][2]:^20.12f}\n'
+    content +='  '.join(types)
+    content +='\n'
+    content +='  '.join(list(map(str,atoms)))
+    content +='\nSelective dynamics\nDirect\n'
+    for i in range(len(Dcoords)):
+        content += f'{" ":^6}{Dcoords[i][0]:^20.12f}{Dcoords[i][1]:^20.12f}{Dcoords[i][2]:^20.12f}{"T  T  T":>13}\n'
+    o = open(filename,'w')
+    o.write(content)
+    o.close()
+    return 1
+
 def readDOSCAR(filename, ISPIN):
     # LORBIT = 11 format
     o = open(filename,'r')
@@ -122,6 +144,28 @@ def fit_density(target_rho, lat, types, atoms):
    given_rho = Tot_mass/V/Na*10**24
    fit_lat = (given_rho/target_rho)**(1/3.)*lat
    return fit_lat
+
+def gen_adjMAT(lat, Ccoords, cutR):
+    N = len(Ccoords)
+    adjMAT = np.zeros((N,N))
+    for i in range(N):
+        for j in range(N):
+            if j>i:
+                if get_DIST(lat,Ccoords[i],Ccoords[j]) < cutR:
+                    adjMAT[i][j] = 1
+    return adjMAT
+
+def get_DIST(lat, pos_i, pos_j):
+    #pos_X : Cartesian coordinates
+    dist = 999
+    for a in range(-1,2):
+        for b in range(-1,2):
+            for c in range(-1,2):
+                tmp = pos_j + np.matmul(np.array([a,b,c]),lat)
+                eval_dist = np.linalg.norm(pos_i-tmp)
+                if eval_dist < dist:
+                    dist = eval_dist
+    return dist
 
 #--------------------------------------------------------------------------------------
 if __name__ == "__main__":
