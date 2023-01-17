d4381bf1935c1a4350e3e45273c748db371e5d2c
Dongheon <zwqa1234@snu.ac.kr>
2023-01-17 11:12:08 +0900

    Update MSD_xdat.py

--- vasp_util/MSD_xdat.py
+++ vasp_util/MSD_xdat.py
@@ -1,11 +1,10 @@
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
 
 def main():
     atoms = []
-    MSD = [0.0]
-    step = 10
+    step  = 10
+    n = 0
 
     lat = np.zeros((3,3))
     o = enumerate(open(sys.argv[1],'r'))
@@ -16,36 +15,41 @@ def main():
     ntypes = list(map(int,next(o)[1].split()))
     NIONS = sum(ntypes)
     if len(atoms) == 0: atoms = range(NIONS)
-    ref_coords = np.zeros((len(atoms,3))
-    old_coords = np.zeros((len(atoms,3))
-    new_coords = np.zeros((len(atoms,3))
+    atom_MSD = np.array([[0.0]*len(atoms)])
+    ref_coords = np.zeros((len(atoms),3))
+    old_coords = np.zeros((len(atoms),3))
+    new_coords = np.zeros((len(atoms),3))
     displacement = np.zeros((len(atoms),3))
 
     # Reference frame 0
     next(o);
     for i in range(NIONS):
+        line = next(o)
         if i in atoms:
-            ref_coords[atoms.index(i)] = np.matmul(list(map(float,next(o)[1].split())),lat)
+            ref_coords[atoms.index(i)] = np.matmul(list(map(float,line[1].split())),lat)
     old_coords = np.copy(ref_coords)
 
-    # frame ~
+    # frame every "step"
+    fr = 1
     for idx, line in o:
-        for i in range(step*(NIONS+1)): next(o);
-        next(o);
-        for i in range(NIONS):
-            line = next(o)[1]
-            if i in atoms:
+        if line[0]=='D':
+            idx,line = next(o)
+            fr +=1
+            n = 0
+        if fr % step == 0:
+            if n in atoms:
                 tmp = np.matmul(list(map(float,line.split())),lat)
-                res = get_DIST(lat, old_coords[atoms.index(i)], tmp)
-                displacement[atoms.index(i)] += res[1]
-                new_coords[atoms.index(i)] = old_coords[atoms.index(i)] + res[1]
-
-        old_coords = np.copy(new_coords)
-        MSD.append(np.mean((displacement)**2))
-
-    plt.plot(MSD)
-    plt.tight_layout()
-    plt.show()
+                res = get_DIST(lat, old_coords[atoms.index(n)], tmp)
+                displacement[atoms.index(n)] += res[1]
+                new_coords[atoms.index(n)] = old_coords[atoms.index(n)] + res[1]
+            n += 1
+            if n == NIONS:
+                old_coords = np.copy(new_coords)
+                atom_MSD = np.linalg.norm(displacement,axis=1)**2
+                p_line = f'{fr:<16d}'
+                for i in range(len(atom_MSD)):
+                    p_line += f'{atom_MSD[i]:^20.12f}'
+                print(p_line)
 
 def get_DIST(lat, pos_i, pos_j):
     #pos_X : Cartesian coordinates
@@ -53,7 +57,7 @@ def get_DIST(lat, pos_i, pos_j):
     for a in range(-1,2):
         for b in range(-1,2):
             for c in range(-1,2):
-                tmp = pos_j + np.matmul(np.array([a,b,c]),lat)
+                tmp = pos_j + a*lat[0] + b*lat[1] + c*lat[2]
                 eval_dist = np.linalg.norm(pos_i-tmp)
                 if eval_dist < dist:
                     dist = eval_dist
