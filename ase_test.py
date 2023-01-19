4dd1d785283b9f6d320a5bd38ef7cfcb7e189e06
Dongheon <zwqa1234@snu.ac.kr>
2023-01-19 09:23:29 +0900

    Delete ase_test.py

--- ase_test.py
+++
@@ -1,9 +0,0 @@
-# @shell
-# $ export ASE_LAMMPSRUN_COMMAND=/path/to/lmp_binary
-# 
-# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6719897/
-import ase
-from ase.calculators.lammpsrun import LAMMPS
-
-
-lmp = lammps()
