2718903766eff31834e3839f2d892e82779ee59c
Dongheon <zwqa1234@snu.ac.kr>
2023-02-06 15:26:09 +0900

    Create Example_ASE_w_MPI.py

---
+++ vasp_util/Example_ASE_w_MPI.py
@@ -0,0 +1,24 @@
+import ase
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+rsize= comm.Get_size()
+
+test_in  = 'POSCAR_in'
+test_out = 'POSCAR_out'
+
+# !mpirun -np $NN python (*.py)
+#
+# Case 1 - Automatic broadcast in ASE
+#
+structure = ase.io.read(test_in,index=':',format='vasp')
+ase.io.write(test_out,images=structure,format='vasp')
+
+
+# Case 2 - Manual parallelization
+# parallel = True or False
+if rank==0:
+  structure = ase.io.read(test_in,index=':',format='vasp',parallel=False)
+  ase.io.write(test_out,images=structure,format='vasp',parallel=False)
+else:
+  pass
