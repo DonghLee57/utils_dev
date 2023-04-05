fa230d4f910601843317c0971cab3e3ba9837fd5
Dongheon <zwqa1234@snu.ac.kr>
2023-04-05 17:29:33 +0900

    Update XDAT2POS.py

--- example_code/XDAT2POS.py
+++ example_code/XDAT2POS.py
@@ -7,8 +7,8 @@ if len(sys.argv[2:]) > 1:
     [start,end,step] = list(map(int,sys.argv[2:]))
     if end == -1: 
         end = len(XDAT)
-     for i in range(start,end+1,step):
-         ase.io.write(f'POSCAR_{i}',images=XDAT[i-1],format='vasp')
+    for i in range(start,end+1,step):
+        ase.io.write(f'POSCAR_{i}',images=XDAT[i-1],format='vasp')
        
 elif len(sys.argv[2:]) == 1:
     fr = int(sys.argv[2])
