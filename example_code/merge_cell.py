3f65e0fd53b122561d0b69e87197a28b98aaeca4
Dongheon <zwqa1234@snu.ac.kr>
2023-04-07 09:58:06 +0900

    Update merge_cell.py

--- example_code/merge_cell.py
+++ example_code/merge_cell.py
@@ -1,6 +1,18 @@
 # Python3.x
+# Reference - J. Phys.: Condens. Matter 29 (2017) 185901
+# Algorithm
+# 1. Construction of the surface supercells
+# 2. Rotational alignment
+# 3. Supercell matching
+# 4. Acceptance of the interface supercell
 
-# different lattice vectors
-# optimal vector manipulation 
-# using ase
-# as small as possible
+import sys
+import numpy as np
+import ase.io
+import ase.build
+
+NMAX, MMAX = 6,6
+[str_bot,str_top] = ase.io.read(sys.argv[1:3], format='vasp')
+[bot_hkl, top_hkl] = [sys.argv[3], sys.argv[4]]
+
+# 1. Construction of the surface supercells
