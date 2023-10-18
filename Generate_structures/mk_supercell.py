4b8ee8c49e37a3cc523aa660a3e1bcd0d5591c47
Dongheon <zwqa1234@gmail.com>
2023-10-18 14:55:48 +0900

    Update and rename mk_supercell.py to mk_supercell.py

--- Generate_structures/mk_supercell.py
+++ Generate_structures/mk_supercell.py
@@ -1,16 +1,12 @@
 # python3.X
-
 import sys
 import numpy as np
-import ase.io
+from ase.io import read, write
 import ase.build.supercells
-from ase.constraints import FixAtoms
-
-unit = ase.io.read(sys.argv[1],format='vasp')
+unit = read(sys.argv[1],format='vasp')
 P = np.array([[5, 0, 0],
               [0, 5, 0],
               [0, 0, 5]])
 supercell = ase.build.supercells.make_supercell(unit, P)
 supercell = supercell[supercell.numbers.argsort()]
-supercell.set_constraint(FixAtoms([-1]))
-ase.io.write('POSCAR-supercell', images=supercell, format='vasp')
+write('POSCAR-supercell', images=supercell, format='vasp')
