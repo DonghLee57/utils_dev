ff2f414fa0b160e1f71653984eb66b21cf20b191
Dongheon <zwqa1234@gmail.com>
2023-12-15 13:43:35 +0900

    Update LAMMPS_simulator.py

--- test_wrap/LAMMPS_simulator.py
+++ test_wrap/LAMMPS_simulator.py
@@ -12,26 +12,28 @@ def main():
   POT = sys.argv[2]
   pot_type = 'NNP'
   # If not NNP,
-  #my_symbol = [['A'],['B']]
+  symbols = [['A'],['B']]
   
   if pot_type == 'NNP':
-    REF_FILE = './POSCAR'
+    REF_FILE = './POSCAR_generated.vasp'
     symbols = get_sorted_symbols(REF_FILE)
     tmp = read(REF_FILE, format='vasp')
     write('test.lammps', images=tmp, format='lammps-data')
     
   my = SIMULATOR(POT, pot_type)
-  # def by_script(self, SCRIPT):
-  #my.by_script('test.in')
+  #def script_header(self, FILE, symbols, unit='metal', style='atomic', pbc=[1,1,1]):
+  #def MINIMIZE(self, cell=False):
+  #def ANNEAL(self, T, TIME=500, ensemble='nvt'):
+  #def QUENCH(self, T_init, T_end=300.0, Qrate=10.0, ensemble='nvt'):
+  my.script_header('test.lammps', symbols,'metal','atomic',[1,1,1])
+  my.MINIMIZE()
+  my.ANNEAL(1000, 10)
+  my.QUENCH(1000)
+  my.MINIMIZE(cell=True)
 
-  # def ANNEAL(self, FILE, symbols, T, TIME=500, ensemble='nvt', min_init=False, min_end=False, out=None):
-  #my.ANNEAL()
+  #def RUN_SCRIPT(self, SCRIPT=self.SCRIPT):
+  my.RUN_SCRIPT(my.SCRIPT)
 
-  # def MQ(self, FILE, symbols, T_melt, T_init, T_end=300.0, Qrate=10.0, ensemble='nvt', min_init=False, min_end=False, out=None):
-  #my.MQ()
-
-  # def NEB(self, FILE, symbols, NEBSTEP):
-  #my.NEB()
 
 ###
 class SIMULATOR:
@@ -43,101 +45,112 @@ class SIMULATOR:
     if self.pot_type == 'NNP':
       self.symbols = subprocess.check_output(['head',f'{self.POT}','-n','1'],universal_newlines=True).split()[1:]
       self.NTYPES = len(self.symbols)
-    elif self.pot_type == 'tersoff':
+    elif self.pot_type in ['tersoff','sw'] :
       self.symbols = None
     self.SCRIPT = 'script.in'
+    self.EXEC = 0
+    self.LOG = 'simulator.log'
+    self.create_vel = True
+    self.write_dump = True
+    self.run_MD = True
 
-  def by_script(self, SCRIPT):
-    res = subprocess.check_output([MPIRUN,'-n',NNCORE,LAMMPS,'-in',SCRIPT],universal_newlines=True)
-    with open(f'log.x','w') as o: o.write(res)
-    return 0
-
-  def ANNEAL(self, FILE, symbols, T, TIME=500, ensemble='nvt', min_init=False, min_end=False, out=None):
+  def script_header(self, FILE, symbols, unit='metal', style='atomic', pbc=[1,1,1]):
+    if self.EXEC == 0:
+        with open(self.LOG,'w') as o: o.write(f'{self.EXEC:4d} : Initialize "{SCRIPT}"\n')
+    else:
+        with open(self.LOG,'a') as o: o.write(f'\n{self.EXEC:4d} : Initialize "{SCRIPT}"\n')
+    header  = f'processors\t* * * grid numa\n'
+    header  = f'units\t\t{unit}\n'
+    header += f'atom_style\t{style}\n'
+    PBC = []
+    for _, b in enumerate(pbc):
+        if b: PBC.append('p')
+        else: PBC.append('f')
+    header += f'boundary\t{" ".join(PBC)}\n'
+    header += f'box\t\ttilt large\n'
+    header += f'read_data\t{FILE}\n\n'
+    header += set_pot(self.POT, self.pot_type, symbols, self.symbols)
     if self.pot_type == 'NNP': NNP_prepare(FILE, self.NTYPES)
+    header += f'thermo\t\t{self.DUMPSTEP} \n'
+    header +=  'thermo_modify\tlost ignore flush yes\n\n'
+    with open(self.SCRIPT,'w') as o: o.write(header)
+
+  def RUN_SCRIPT(self, SCRIPT):
+    self.EXEC += 1
+    res = subprocess.check_output([MPIRUN, '-n', NNCORE, LAMMPS, '-in', SCRIPT],universal_newlines=True)
+    with open('0_log.x','w') as o: o.write(res)
+    if self.EXEC == 1:
+        with open(self.LOG,'w') as o: o.write(f'{self.EXEC:4d} : LAMMPS running with "{SCRIPT}"\n')
+    else:
+        with open(self.LOG,'a') as o: o.write(f'\n{self.EXEC:4d} : LAMMPS running with "{SCRIPT}"\n')
+
+  def MINIMIZE(self, cell=False):
+    self.EXEC +=1
+    with open(self.SCRIPT,'a') as o:
+      if cell: o.write('fix\t\t1 all box/relax iso 0.0 vmax 0.001 fixedpoint 0 0 0\n')
+      o.write('minimize 1e-6 1e-10 10000 100000\n')
+      if cell: o.write('unfix\t\t1\n')
+      o.write(f'write_data\t{self.EXEC}_MINIMIZE.lammps\n\n')
+    if self.EXEC == 1:
+        with open(self.LOG,'w') as o: o.write(f'{self.EXEC:4d} : Minimize the structure.\n')
+    else:
+        with open(self.LOG,'a') as o: o.write(f'\n{self.EXEC:4d} : Minimize the structure.\n')
+
+  def ANNEAL(self, T, TIME=500, ensemble='nvt'):
+    self.EXEC += 1
     It = int(TIME/self.TIMESTEP)
-    with open(self.SCRIPT,'w') as o:
-      o.write('processors\t* * * grid numa\n')
-      o.write('units\t\tmetal\n')
-      o.write('atom_style\tatomic\n')
-      o.write('boundary\tp p p\n')
-      o.write('box\t\ttilt large\n')
-      o.write(f'read_data\t{FILE}\n\n')
-      o.write(set_pot(self.POT, self.pot_type, symbols, self.symbols))
-      o.write(f'thermo\t\t{self.DUMPSTEP} \n')
-      o.write('thermo_modify\tlost ignore flush yes\n\n')
-      if min_init:
-        o.write('fix 1 all box/relax iso 0.0 vmax 0.001 fixedpoint 0 0 0\n')
-        o.write('minimize 1e-6 1e-10 10000 100000\n')
-        if out == None: o.write(f'write_data\tmin_init.lammps\n')
-        else:           o.write(f'write_data\tmin_init_{out}.lammps\n')
-      o.write(f'velocity\tall create {T} {np.random.randint(low=1,high=100)} dist gaussian\n')
+    with open(self.SCRIPT,'a') as o:
+      if self.create_vel:
+        o.write(f'velocity\tall create {T} {np.random.randint(low=1,high=1000)} dist gaussian\n')
+        self.create_vel = False
       if ensemble =='nvt':   o.write(f'fix\t\tTHERMOSTAT all nvt temp {T} {T} 1\n')
       elif ensemble =='npt': o.write(f'fix\t\tTHERMOSTAT all npt temp {T} {T} 1 iso 0.0 0.0 1000.0 fixedpoint 0 0 0\n')
-      o.write(f'timestep {self.TIMESTEP:.4f}\n')
-      o.write(f'dump\t\tDUMP all custom {self.DUMPSTEP} dump_anneal_{T:d}.lammpstrj id type x y z\n')
-      o.write('dump_modify\tDUMP sort id\n\n')
+      if self.write_dump:
+        o.write(f'dump\t\tDUMP all custom {self.DUMPSTEP} dump.lammpstrj id type x y z\n')
+        o.write('dump_modify\tDUMP sort id\n\n')
+        self.write_dump = False
+      if self.run_MD:
+        o.write(f'timestep\t\t{self.TIMESTEP:.4f}\n')
+        self.run_MD = False
       o.write(f'run\t\t{It}\n')
-      o.write(f'write_data\t{T:d}.lammps\n')
-      if min_end:
-        o.write('unfix THERMOSTAT\n')
-        o.write('fix 1 all box/relax iso 0.0 vmax 0.001 fixedpoint 0 0 0\n')
-        o.write('minimize 1e-6 1e-10 10000 100000\n')
-        if out == None: o.write(f'write_data\tmin_end.lammps\n')
-        else:           o.write(f'write_data\tmin_end_{out}.lammps\n')
-    res = subprocess.check_output([MPIRUN,'-n',NNCORE,LAMMPS,'-in',self.SCRIPT],universal_newlines=True)
-    with open(f'log_anneal_{T:d}.x','w') as o: o.write(res)
+      o.write(f'unfix\t\tTHERMOSTAT\n')
+      o.write(f'write_data\t{self.EXEC}_ANNEAL.lammps\n\n')
+    if self.EXEC == 1:
+        with open(self.LOG,'w') as o: o.write(f'{self.EXEC:4d} : Annealing at {T} K during {TIME} ps.\n')
+    else:
+        with open(self.LOG,'a') as o: o.write(f'{self.EXEC:4d} : Annealing at {T} K during {TIME} ps.\n')
 
-  def MQ(self, FILE, symbols, T_melt, T_init, T_end=300.0, Qrate=10.0, ensemble='nvt', min_init=False, min_end=False, out=None):
-    if self.pot_type == 'NNP': NNP_prepare(FILE, self.NTYPES)
-    It = int((T_init-T_end)/(Qrate*self.TIMESTEP))
-    with open(self.SCRIPT,'w') as o:
-      o.write('processors\t* * * grid numa\n')
-      o.write('units\t\tmetal\n')
-      o.write('atom_style\tatomic\n')
-      o.write('boundary\tp p p\n')
-      o.write('box\t\ttilt large\n')
-      o.write(f'read_data\t{FILE}\n\n')
-      o.write(set_pot(self.POT, self.pot_type, symbols, self.symbols))
-      o.write(f'thermo\t\t{self.DUMPSTEP} \n')
-      o.write('thermo_modify\tlost ignore flush yes\n\n')
-      if min_init:
-        o.write('fix 1 all box/relax iso 0.0 vmax 0.001 fixedpoint 0 0 0\n')
-        o.write('minimize 1e-6 1e-10 10000 100000\n')
-        if out == None: o.write(f'write_data\tmin_init.lammps\n')
-        else:           o.write(f'write_data\tmin_init_{out}.lammps\n')
-      o.write(f'velocity\tall create {T_melt} {np.random.randint(low=1,high=100)} dist gaussian\n')
-      if ensemble =='nvt':   o.write(f'fix\t\tTHERMOSTAT all nvt temp {T_melt} {T_melt} 1\n')
-      elif ensemble =='npt': o.write(f'fix\t\tTHERMOSTAT all npt temp {T_melt} {T_melt} 1 iso 0.0 0.0 1000.0 fixedpoint 0 0 0\n')
-      o.write(f'dump\t\tDUMP all custom {self.DUMPSTEP} dump_mq.lammpstrj id type x y z\n')
-      o.write('dump_modify\tDUMP sort id\n\n')
-      o.write(f'timestep {self.TIMESTEP:.4f}\n')
-      o.write(f'run\t\t50000\n')
-      o.write('unfix THERMOSTAT\n')
+  def QUENCH(self, T_init, T_end=300.0, Qrate=10.0, ensemble='nvt'):
+    self.EXEC += 1
+    It = int(np.fabs(T_init-T_end)/(Qrate*self.TIMESTEP))
+    with open(self.SCRIPT,'a') as o:
+      if self.create_vel:
+        o.write(f'velocity\tall create {T} {np.random.randint(low=1,high=1000)} dist gaussian\n')
+        self.create_vel = False
+      if self.write_dump:
+        o.write(f'dump\t\tDUMP all custom {self.DUMPSTEP} dump.lammpstrj id type x y z\n')
+        o.write('dump_modify\t\tDUMP sort id\n\n')
+        self.write_dump = False
       if ensemble =='nvt':   o.write(f'fix\t\tTHERMOSTAT all nvt temp {T_init} {T_end} 1\n')
       elif ensemble =='npt': o.write(f'fix\t\tTHERMOSTAT all npt temp {T_init} {T_end} 1 iso 0.0 0.0 1000.0 fixedpoint 0 0 0\n')
+      if self.run_MD:
+        o.write(f'timestep\t\t{self.TIMESTEP:.4f}\n')
+        self.run_MD = False
       o.write(f'run\t\t{It}\n')
-      if min_end:
-        o.write('unfix THERMOSTAT\n')
-        o.write('fix 1 all box/relax iso 0.0 vmax 0.001 fixedpoint 0 0 0\n')
-        o.write('minimize 1e-6 1e-10 10000 100000\n')
-        if out == None: o.write(f'write_data\tmin_end.lammps\n')
-        else:           o.write(f'write_data\tmin_end_{out}.lammps\n')
-    res = subprocess.check_output([MPIRUN,'-n',NNCORE,LAMMPS,'-in',self.SCRIPT],universal_newlines=True)
-    with open(f'log_mq.x','w') as o: o.write(res)
-  
+      o.write(f'unfix\t\tTHERMOSTAT\n')
+      o.write(f'write_data\t{self.EXEC}_QUENCH.lammps\n\n')
+    if self.EXEC == 1:
+        with open(self.LOG,'w') as o: o.write(f'{self.EXEC:4d} : Quenching with {Qrate} K/ps from {T_init} to {T_end}.\n')
+    else:
+        with open(self.LOG,'a') as o: o.write(f'{self.EXEC:4d} : Quenching with {Qrate} K/ps from {T_init} to {T_end}.\n')
+
   def NEB(self, FILE, symbols, NEBSTEP):
+    self.EXEC += 1
+    """
     # Check running
-    if self.pot_type == 'NNP': NNP_prepare(FILE, self.NTYPES)
     with open(self.SCRIPT,'w') as o:
-      o.write('processors\t* * * grid numa\n')
-      o.write('units\t\tmetal\n')
-      o.write('atom_style\tatomic\n')
-      o.write('boundary\tp p p\n')
-      o.write('box\t\ttilt large\n')
       o.write(f'read_data\t{FILE}\n\n')
       o.write(set_pot(self.POT, self.pot_type, symbols, self.symbols))
-      o.write(f'thermo\t\t{self.DUMPSTEP} \n')
-      o.write('thermo_modify\tlost ignore flush yes\n\n')
       o.write(f'variable\t\tu uloop {NEBSTEP}\n')
       o.write(f'reset_timestep 0\n')
       o.write(f'timestep {self.TIMESTEP:.4f}\n')
@@ -146,10 +159,8 @@ class SIMULATOR:
       o.write('### quickmin fire cg sd hftn\n')
       o.write('min_style\t\tquickmin\n')
       o.write('neb\t\t0.0 0.01 100 100 10 each coords_$i.lammps\n')
-    res = subprocess.check_output([MPIRUN,'-n',NNCORE,LAMMPS,'-in',self.SCRIPT],universal_newlines=True)
-    with open(f'log_NEB.x','w') as o: o.write(res)
-        
-###
+    """
+    
 def C2K(T):
   return T+273.15
   
