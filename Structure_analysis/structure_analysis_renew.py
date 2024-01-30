d8c65641c18f3122fa322f2a563628c37a676ab6
Dongheon <zwqa1234@gmail.com>
2024-01-30 09:44:12 +0900

    Update structure_analysis_renew.py

--- Structure_analysis/structure_analysis_renew.py
+++ Structure_analysis/structure_analysis_renew.py
@@ -1,7 +1,7 @@
 import sys, os, glob
 import numpy as np
 import itertools
-from ase.io import read
+from ase.io import read, write
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
@@ -12,9 +12,9 @@ from scipy.ndimage import gaussian_filter1d
 
 def main():
     my = StructureAnalysis('POSCAR', 'vasp')
-    #my = StructureAnalysis('XDATCAR', 'vasp-xdatcar',index=slice(0,1000,10))
+    #my = StructureAnalysis('XDATCAR', 'vasp-xdatcar',index=slice(0,1000,10), compress=True)
     #my = StructureAnalysis('my.lammps', 'lammps-data')
-    #my = StructureAnalysis('dump.lammpstrj', 'lammps-dump-text', index=slice(0,1000,10))
+    #my = StructureAnalysis('dump.lammpstrj', 'lammps-dump-text', index=slice(0,1000,10), compress=True)
     
     rdf  = my.calculate_rdf()
     prdf = my.calculate_prdf()
@@ -23,8 +23,9 @@ def main():
 
 ###
 class StructureAnalysis:
-    def __init__(self, filename, fileformat, index='-1'):
+    def __init__(self, filename, fileformat, index='-1', compress=None):
         self.structure = read(filename, index=index, format=fileformat)
+        if compress: write('INPUT_STR.vasp',images=self.structure, format='vasp-xdatcar')
 
     def calculate_rdf(self, dr=0.02, r_max=None):
         nimg = len(self.structure)
@@ -52,27 +53,27 @@ class StructureAnalysis:
         bins = np.arange(dr/2, r_max, dr)
         distances = []
         for n, atoms in enumerate(self.structure):
-        # indentation
-        for i in range(len(atoms)):
-            if atoms[i].symbol != element1:
-                continue
-            for j in range(len(atoms)):
-                if i != j and atoms[j].symbol == element2:
-                    # Compute distance between atoms i and j
-                    distance = atoms.get_distance(i, j, mic=True)
-                    if distance < r_max:
-                        distances.append(distance)
-        res, bin_edges = np.histogram(distances, bins=bins)
-        n_element1 = len([atom for atom in atoms if atom.symbol == element1])
-        n_element2 = len([atom for atom in atoms if atom.symbol == element2])
-        rdf += res / (n_element1 * n_element2 / atoms.get_volume() * 4 * np.pi * dr * bin_edges[:-1]**2)
-        # indentation
+            for i in range(len(atoms)):
+                if atoms[i].symbol != element1:
+                    continue
+                for j in range(len(atoms)):
+                    if i != j and atoms[j].symbol == element2:
+                        # Compute distance between atoms i and j
+                        distance = atoms.get_distance(i, j, mic=True)
+                        if distance < r_max:
+                            distances.append(distance)
+            res, bin_edges = np.histogram(distances, bins=bins)
+            n_element1 = len([atom for atom in atoms if atom.symbol == element1])
+            n_element2 = len([atom for atom in atoms if atom.symbol == element2])
+            rdf += res / (n_element1 * n_element2 / atoms.get_volume() * 4 * np.pi * dr * bin_edges[:-1]**2)
         return bin_edges[1:], rdf
     
-
     def calculate_adf(self, bins=100, r_max=None):
+        nimg = len(self.structure)
+        rdf = np.zeros(bins)
         if r_max is None:
             r_max = atoms.get_cell().diagonal().min() / 2  # Half the smallest cell dimension
+        bins = np.arange(dr/2, r_max, dr)
         angles = []
         for i in range(len(atoms)):
             neighbors_i = atoms.get_neighbors(i, r_max)[0]
@@ -92,7 +93,7 @@ class StructureAnalysis:
         # Create a histogram of angles
         adf, bin_edges = np.histogram(angles, bins=bins, range=(0, 180), density=True)
         return bin_edges[1:], adf     
-        
+
 ###
 if __name__ == "__main__":
     main()
